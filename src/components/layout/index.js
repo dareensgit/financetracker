26061438b2bc9ea8af91fe2dd181462708ddd7d6
Maya321-wq <s-maya.akl@zewailcity.edu.eg>
2025-12-16 23:08:03 +0200

    feat(layout): add layout wrapper components

---
+++ src/components/layout/index.js
@@ -0,0 +1,6 @@
+// Layout Components exports
+export { default as Container } from './Container'
+export { default as Card } from './Card'
+export { default as Footer } from './Footer'
+export { default as Header } from './Header'
+
